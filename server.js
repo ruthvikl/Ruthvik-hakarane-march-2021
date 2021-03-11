73db9efc37978831003aa17b237eca4689459103
ruthvikl <48211227+ruthvikl@users.noreply.github.com>
2021-03-12 00:09:47 +0530

    Added a Procfile.

--- server.js
+++ server.js
@@ -2,20 +2,17 @@ var express = require('express');
 var app = express();
 const path = require("path")
 var fs = require("fs");
-const axios = require('axios');
 
 app.locals.users = require('./info.json')
 app.locals.project = require('./projects.json')
 app.get('/info', function (req, res) {
    fs.readFile( __dirname + "/" + "info.json", 'utf8', function (err, data) {
-    //   console.log( data );
       res.end( data );
    });
 })
 
 app.get('/projects', function (req, res) {
    fs.readFile( __dirname + "/" + "projects.json", 'utf8', function (err, data) {
-      console.log( data );
       res.end( data );
    });
 })
@@ -28,7 +25,6 @@ app.get('/project/', function (req, res){
 })
 // set view engine
 app.set("view engine","ejs")
-// app.set("views", path.resolve(__dirname,"views/ejs"))  if front end files placed inside views folder inside ejs folder
 
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))   
