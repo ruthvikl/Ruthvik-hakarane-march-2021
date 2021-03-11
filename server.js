9ad70bd627f7290ff1efa64dacb005ebd96d487b
ruthvikl <48211227+ruthvikl@users.noreply.github.com>
2021-03-11 23:54:43 +0530

    Added a Procfile.

--- server.js
+++ server.js
@@ -2,31 +2,37 @@ var express = require('express');
 var app = express();
 const path = require("path")
 var fs = require("fs");
-app.get('/', function (req, res) {
+const axios = require('axios');
+
+app.locals.users = require('./info.json')
+app.locals.project = require('./projects.json')
+app.get('/info', function (req, res) {
    fs.readFile( __dirname + "/" + "info.json", 'utf8', function (err, data) {
     //   console.log( data );
       res.end( data );
    });
 })
 
-// app.get('/projects', function (req, res) {
-//    fs.readFile( __dirname + "/" + "projects.json", 'utf8', function (err, data) {
-//       console.log( data );
-//       res.end( data );
-//    });
-// })
+app.get('/projects', function (req, res) {
+   fs.readFile( __dirname + "/" + "projects.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+app.get('/', function (req, res){ 
+    res.render("profile");
+})
 
-// app.get('/', function (req, res){
-   
-// })
+app.get('/project/', function (req, res){ 
+    res.render("project");
+})
 // set view engine
 app.set("view engine","ejs")
 // app.set("views", path.resolve(__dirname,"views/ejs"))  if front end files placed inside views folder inside ejs folder
 
-//loading assets like css js
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
-// app.use('/img', express.static(path.resolve(__dirname, "assets/img")))  if u have images in the module 
-app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))   
+// app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 
 
